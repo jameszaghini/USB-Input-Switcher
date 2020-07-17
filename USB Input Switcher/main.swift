c5ae069cea68c6698c02d8a2f67b89e66fe8b95f
James Zaghini <james@zaghini.com>
2020-07-18 09:37:25 +1000

    Initial Commit

---
+++ USB Input Switcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  USB Input Switcher
+//
+//  Created by James Zaghini on 18/7/20.
+//  Copyright © 2020 James Zaghini. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
